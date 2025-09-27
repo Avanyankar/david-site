211a803bccf29093a1856af306828b4cb3f86301
Karen Avanyan <IProgrammist.1@gmail.com>
2025-09-28 00:34:05 +0300

    Finally add tailwind

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # david-s-site
+
+tailwindcss -i ./static/src/styles.css -o ./static/dist/styles.css --watch
+tailwindcss -i ./static/src/styles.css -o ./static/dist/styles.css --minify
--- source/main.py
+++ source/main.py
@@ -1,16 +1,29 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse, HTMLResponse
+from pathlib import Path
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static/dist"), name="static")
+current_dir = Path(__file__).parent
+
+static_dir = current_dir.parent / "static"
+
+app.mount("/static", StaticFiles(directory=str(static_dir)), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/favicon.ico")
+async def favicon():
+    return FileResponse("static/images/icons/favicon.ico")
+
+@app.get("/.well-known/appspecific/com.chrome.devtools.json")
+async def chrome_devtools():
+    return {"message": "No DevTools configuration"}
 
 if __name__ == "__main__":
     import uvicorn
