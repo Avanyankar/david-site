5e3ebbeb2241f030501d9b3fc2927432c87736b0
Karen Avanyan <IProgrammist.1@gmail.com>
2025-10-09 12:51:16 +0300

    add prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./templates/**/*.html"
+    "./templates/**/*.html",
+    "./src/**/*.js"
   ],
   theme: {
     extend: {},
